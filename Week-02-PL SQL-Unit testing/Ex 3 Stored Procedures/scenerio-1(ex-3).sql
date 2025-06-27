6471aa6e5a4a990bb31ddc757a03f7cfb2c4ddbd
Balaji M <159635676+mrbalajibtech@users.noreply.github.com>
2025-06-27 13:51:20 +0530

    Delete Week-02-PL SQL-Unit testing/Ex 3 Stored Procedures directory

--- Week-02-PL SQL-Unit testing/Ex 3 Stored Procedures/scenerio-1(ex-3).sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest IS
-BEGIN
-    UPDATE Accounts
-    SET Balance = Balance + (Balance * 0.01),
-        LastModified = SYSDATE
-    WHERE AccountType = 'Savings';
-    COMMIT;
-END;
-/
